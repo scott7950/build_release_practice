ffc72a3a3782460a5074615d8637217aca3e844d
scott7950 <scott0926@outlook.com>
2015-07-25 09:11:32 -0400

    update include file
    update include file

--- static_library/no_archive/static1/static_lib1.cpp
+++ static_library/no_archive/static1/static_lib1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "static_lib1.h"
+#include "static_lib2.h"
 
 void display1() {
     std::cout << "*** In Static Lib 1 ***" << std::endl;
--- static_library/no_archive/static1/static_lib1.h
+++ static_library/no_archive/static1/static_lib1.h
@@ -1,8 +1,6 @@
 #ifndef __STATIC_LIB1_H__
 #define __STATIC_LIB1_H__
 
-#include "static_lib2.h"
-
 extern void display1();
 
 #endif
